b73dce8264bee15453710a3d51eb34c890c79eaa
svenlange <sv3n.lange@gmail.com>
2013-01-30 23:38:30 +0100

    generated parser and psi files

---
+++ gen/za/co/svenlange/intellij/xtend/psi/XtendVisitor.java
@@ -0,0 +1,18 @@
+// This is a generated file. Not intended for manual editing.
+package za.co.svenlange.intellij.xtend.psi;
+
+import org.jetbrains.annotations.*;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.PsiElement;
+
+public class XtendVisitor extends PsiElementVisitor {
+
+  public void visitProperty(@NotNull XtendProperty o) {
+    visitPsiElement(o);
+  }
+
+  public void visitPsiElement(@NotNull PsiElement o) {
+    visitElement(o);
+  }
+
+}
