e022d8c511d6712b26ee1d7eb5cd418d74564a99
svenlange <sv3n.lange@gmail.com>
2013-04-26 12:51:17 +0200

    first successful compiler impl

--- jps-plugin/src/za/co/svenlange/intellij/xtend/jps/incremental/XtendBuilder.java
+++ jps-plugin/src/za/co/svenlange/intellij/xtend/jps/incremental/XtendBuilder.java
@@ -1,36 +1,95 @@
 package za.co.svenlange.intellij.xtend.jps.incremental;
 
+import com.google.common.collect.Iterables;
+import com.google.inject.Injector;
+import com.intellij.execution.process.BaseOSProcessHandler;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.util.ArrayUtil;
+import org.eclipse.xtend.core.XtendInjectorSingleton;
+import org.eclipse.xtend.core.compiler.batch.XtendBatchCompiler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.jps.ModuleChunk;
+import org.jetbrains.jps.builders.BuildTargetType;
 import org.jetbrains.jps.builders.DirtyFilesHolder;
 import org.jetbrains.jps.builders.java.JavaSourceRootDescriptor;
-import org.jetbrains.jps.incremental.CompileContext;
-import org.jetbrains.jps.incremental.ModuleBuildTarget;
-import org.jetbrains.jps.incremental.ModuleLevelBuilder;
-import org.jetbrains.jps.incremental.ProjectBuildException;
+import org.jetbrains.jps.builders.logging.BuildLoggingManager;
+import org.jetbrains.jps.builders.logging.ProjectBuilderLogger;
+import org.jetbrains.jps.cmdline.ProjectDescriptor;
+import org.jetbrains.jps.incremental.*;
+import org.jetbrains.jps.incremental.messages.BuildMessage;
+import org.jetbrains.jps.incremental.messages.CompilerMessage;
+import org.jetbrains.jps.model.JpsDummyElement;
+import org.jetbrains.jps.model.JpsUrlList;
+import org.jetbrains.jps.model.java.JpsJavaExtensionService;
+import org.jetbrains.jps.model.java.JpsJavaModuleType;
+import org.jetbrains.jps.model.java.JpsJavaSdkType;
+import org.jetbrains.jps.model.java.compiler.JpsJavaCompilerConfiguration;
+import org.jetbrains.jps.model.library.sdk.JpsSdk;
+import org.jetbrains.jps.model.module.JpsDependenciesList;
+import org.jetbrains.jps.model.module.JpsModule;
+import org.jetbrains.jps.model.module.JpsModuleSourceRoot;
+import org.jetbrains.jps.service.SharedThreadPool;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.*;
+import java.util.concurrent.Future;
 
 import static org.jetbrains.jps.incremental.BuilderCategory.SOURCE_GENERATOR;
 
 public class XtendBuilder extends ModuleLevelBuilder {
 
     private static final Logger LOG = Logger.getInstance("#za.co.svenlange.intellij.xtend.jps.incremental.XtendBuilder");
+    public static final String COMPILER_NAME = "Xtend Compiler";
+    private static final String XTEND_MAIN = "xtend.main";
 
     protected XtendBuilder() {
         super(SOURCE_GENERATOR);
-        LOG.error("XtendBuilder");
     }
 
     @Override
     public ExitCode build(CompileContext context, ModuleChunk chunk, DirtyFilesHolder<JavaSourceRootDescriptor, ModuleBuildTarget> dirtyFilesHolder, OutputConsumer outputConsumer) throws ProjectBuildException, IOException {
-        return null;
+        Injector injector = XtendInjectorSingleton.INJECTOR;
+        XtendBatchCompiler xtendBatchCompiler = injector.getInstance(XtendBatchCompiler.class);
+
+        final String blah = context.getBuilderParameter("blah");
+
+
+//
+//
+//        final List<JpsModuleSourceRoot> sourceRoots = new ArrayList<JpsModuleSourceRoot>();
+//        final List<JpsDependenciesList> dependenciesLists = new ArrayList<JpsDependenciesList>();
+//        for (JpsModule module : chunk.getModules()) {
+//
+//            for (JpsModuleSourceRoot sourceRoot : getSourceRoots()) {
+//                sourceRoots.add(sourceRoot);
+//            }
+//
+//            dependenciesLists.add(module.getDependenciesList());
+//        }
+
+        final JpsModule first = Iterables.getFirst(chunk.getModules(), null);
+        final List<String> contentRootsList = first.getContentRootsList().getUrls();
+        final URL url = new URL(contentRootsList.get(0));
+        final String file = url.getFile();
+        final String path = url.getPath();
+
+
+        xtendBatchCompiler.setOutputPath(file + "/xtend-gen");
+        xtendBatchCompiler.setClassPath("/Users/lange/Projekte/opensource/jetbrains/xtend-intellij/lib/org.eclipse.xtend.standalone-2.4.1.jar");
+        xtendBatchCompiler.setSourcePath(chunk.representativeTarget().getModule().getSourceRoots().get(0).getUrl());
+
+
+        xtendBatchCompiler.compile();
+
+        return ExitCode.OK;
     }
 
     @NotNull
     @Override
     public String getPresentableName() {
-        return "Xtend Compiler";
+        return COMPILER_NAME;
     }
 }
--- jps-plugin/src/za/co/svenlange/intellij/xtend/jps/incremental/XtendBuilderService.java
+++ jps-plugin/src/za/co/svenlange/intellij/xtend/jps/incremental/XtendBuilderService.java
@@ -2,7 +2,6 @@ package za.co.svenlange.intellij.xtend.jps.incremental;
 
 import com.intellij.openapi.diagnostic.Logger;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.jps.builders.BuildTargetType;
 import org.jetbrains.jps.incremental.BuilderService;
 import org.jetbrains.jps.incremental.ModuleLevelBuilder;
 
@@ -13,15 +12,9 @@ public class XtendBuilderService extends BuilderService {
 
     private static final Logger LOG = Logger.getInstance("#za.co.svenlange.intellij.xtend.jps.incremental.XtendBuilderService");
 
-    @Override
-    public List<? extends BuildTargetType<?>> getTargetTypes() {
-        return super.getTargetTypes();    //To change body of overridden methods use File | Settings | File Templates.
-    }
-
     @NotNull
     @Override
     public List<? extends ModuleLevelBuilder> createModuleLevelBuilders() {
-        LOG.error("XtendBuilderService");
         return Arrays.asList(new XtendBuilder());
     }
 
